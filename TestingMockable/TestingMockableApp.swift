cc8f9fc4b66497cbbcf4d511604082b5a5c88097
Chris Cooper <chriscooper1991@gmail.com>
2025-02-12 17:12:39 -0500

    Initial Commit

---
+++ TestingMockable/TestingMockableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingMockableApp.swift
+//  TestingMockable
+//
+//  Created by Chris on 2025-02-12.
+//
+
+import SwiftUI
+
+@main
+struct TestingMockableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestingMockableTests/TestingMockableTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestingMockableTests.swift
+//  TestingMockableTests
+//
+//  Created by Chris on 2025-02-12.
+//
+
+import Testing
+@testable import TestingMockable
+
+struct TestingMockableTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
